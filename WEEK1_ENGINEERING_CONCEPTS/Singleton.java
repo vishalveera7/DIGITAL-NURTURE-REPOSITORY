ca540d4c258c89a3181115060315073d276330e5
Vishal V <vishalv23it@psnacet.edu.in>
2026-07-01 14:16:14 +0530

    Add Logger class implementing Singleton pattern
    Implemented a Logger class using Singleton pattern to ensure a single instance is used for logging messages.

--- WEEK1_ENGINEERING_CONCEPTS/Singleton.java
+++ WEEK1_ENGINEERING_CONCEPTS/Singleton.java
@@ -1,1 +1,29 @@
+public class Logger {
+    private static Logger instance;
 
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
+
+public class SingletonPattern{
+    public static void main(String[] args) {
+        Logger log1 = Logger.getInstance();
+        Logger log2 = Logger.getInstance();
+
+        log1.log("Application started");
+        log2.log("Processing request");
+
+        System.out.println("log1 == log2 ? " + (log1 == log2));
+    }
+}
