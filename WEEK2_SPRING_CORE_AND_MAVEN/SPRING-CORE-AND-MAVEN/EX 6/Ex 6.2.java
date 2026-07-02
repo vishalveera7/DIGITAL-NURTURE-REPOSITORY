cb23138ba0ef4d6d0a3a8d5310117f9842bb652b
Vishal V <vishalv23it@psnacet.edu.in>
2026-07-02 14:30:31 +0530

    Create Ex 6.2.java

---
+++ WEEK2_SPRING_CORE_AND_MAVEN/SPRING-CORE-AND-MAVEN/EX 6/Ex 6.2.java
@@ -0,0 +1,34 @@
+package com.library.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+
+import com.library.repository.BookRepository;
+
+
+
+@Service
+public class BookService {
+
+
+    @Autowired
+    private BookRepository bookRepository;
+
+
+
+    public void addBook() {
+
+
+        System.out.println("BookService running using annotations");
+
+
+        bookRepository.saveBook();
+
+
+    }
+
+
+}
