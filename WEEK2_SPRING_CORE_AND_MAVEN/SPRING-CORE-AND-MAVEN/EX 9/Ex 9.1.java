2af97ceb5a30b92f7c8509c1d7c53efe7f53c483
Vishal V <vishalv23it@psnacet.edu.in>
2026-07-02 14:45:02 +0530

    Create Ex 9.1.java

---
+++ WEEK2_SPRING_CORE_AND_MAVEN/SPRING-CORE-AND-MAVEN/EX 9/Ex 9.1.java
@@ -0,0 +1,69 @@
+package com.library.entity;
+
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Id;
+
+
+
+@Entity
+public class Book {
+
+
+    @Id
+    private int id;
+
+
+    private String bookName;
+
+
+    private String author;
+
+
+
+    public int getId() {
+
+        return id;
+
+    }
+
+
+    public void setId(int id) {
+
+        this.id = id;
+
+    }
+
+
+
+    public String getBookName() {
+
+        return bookName;
+
+    }
+
+
+    public void setBookName(String bookName) {
+
+        this.bookName = bookName;
+
+    }
+
+
+
+    public String getAuthor() {
+
+        return author;
+
+    }
+
+
+    public void setAuthor(String author) {
+
+        this.author = author;
+
+    }
+
+
+}
