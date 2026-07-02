bc9516940329c680dd2945dd0b3554a331c41b22
Vishal V <vishalv23it@psnacet.edu.in>
2026-07-02 14:48:27 +0530

    Create Ex 9.5.java

---
+++ WEEK2_SPRING_CORE_AND_MAVEN/SPRING-CORE-AND-MAVEN/EX 9/Ex 9.5.java
@@ -0,0 +1,30 @@
+package com.library;
+
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+
+public class LibraryManagementApplication {
+
+
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(
+        LibraryManagementApplication.class,args);
+
+
+        System.out.println("Library Management Application Started");
+
+
+    }
+
+
+}
