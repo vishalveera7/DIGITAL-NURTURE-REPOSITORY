0b1d97219675178136adb2a6098043413279c6db
Vishal V <vishalv23it@psnacet.edu.in>
2026-07-02 14:45:44 +0530

    Create Ex 9.2.java

---
+++ WEEK2_SPRING_CORE_AND_MAVEN/SPRING-CORE-AND-MAVEN/EX 9/Ex 9.2.java
@@ -0,0 +1,14 @@
+package com.library.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.library.entity.Book;
+
+
+
+public interface BookRepository extends JpaRepository<Book,Integer>{
+
+
+}
