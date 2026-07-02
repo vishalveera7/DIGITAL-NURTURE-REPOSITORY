f757583da2d099cac5749d67dedec57b8f9468ee
Vishal V <vishalv23it@psnacet.edu.in>
2026-07-02 14:25:18 +0530

    Create Ex 5.2.java

---
+++ WEEK2_SPRING_CORE_AND_MAVEN/SPRING-CORE-AND-MAVEN/EX 5/Ex 5.2.java
@@ -0,0 +1,34 @@
+package com.library.service;
+
+
+import com.library.repository.BookRepository;
+
+
+public class BookService {
+
+
+    private BookRepository bookRepository;
+
+
+
+    public void setBookRepository(BookRepository bookRepository) {
+
+        this.bookRepository = bookRepository;
+
+    }
+
+
+
+    public void addBook() {
+
+
+        System.out.println("BookService running");
+
+
+        bookRepository.saveBook();
+
+
+    }
+
+
+}
