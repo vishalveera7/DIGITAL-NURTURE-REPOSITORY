5f69b307d79acd6ca5a94f7281c7ddea3442180e
Vishal V <vishalv23it@psnacet.edu.in>
2026-07-02 14:31:30 +0530

    Create Ex 6.3.java
    This class initializes the Spring application context and retrieves the BookService bean to add a book.

---
+++ WEEK2_SPRING_CORE_AND_MAVEN/SPRING-CORE-AND-MAVEN/EX 6/Ex 6.3.java
@@ -0,0 +1,42 @@
+package com.library;
+
+
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+import com.library.service.BookService;
+
+
+
+public class LibraryManagementApplication {
+
+
+
+    public static void main(String[] args) {
+
+
+
+        ApplicationContext context =
+
+
+        new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
+
+        BookService service =
+
+
+        context.getBean(BookService.class);
+
+
+
+        service.addBook();
+
+
+
+    }
+
+
+}
