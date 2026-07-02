c71ba644bf5d7bb89ac0620c090ae1272352c129
Vishal V <vishalv23it@psnacet.edu.in>
2026-07-02 14:40:46 +0530

    Add BookService class for managing books

---
+++ WEEK2_SPRING_CORE_AND_MAVEN/SPRING-CORE-AND-MAVEN/EX 8/Ex 8.2.java
@@ -0,0 +1,39 @@
+package com.library.service;
+
+
+import com.library.repository.BookRepository;
+
+
+
+public class BookService {
+
+
+
+    private BookRepository bookRepository;
+
+
+
+    public void setBookRepository(BookRepository bookRepository) {
+
+
+        this.bookRepository = bookRepository;
+
+
+    }
+
+
+
+
+    public void addBook() {
+
+
+        System.out.println("Adding book in service");
+
+
+        bookRepository.saveBook();
+
+
+    }
+
+
+}
