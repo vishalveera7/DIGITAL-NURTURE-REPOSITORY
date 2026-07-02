cd2938c9bfde0dc28d46da749a57f5d98c6a1dd8
Vishal V <vishalv23it@psnacet.edu.in>
2026-07-02 14:28:59 +0530

    Create Ex 6.1.java

---
+++ WEEK2_SPRING_CORE_AND_MAVEN/SPRING-CORE-AND-MAVEN/EX 6/Ex 6.1.java
@@ -0,0 +1,19 @@
+package com.library.repository;
+
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class BookRepository {
+
+
+    public void saveBook() {
+
+
+        System.out.println("Book saved successfully");
+
+
+    }
+
+
+}
